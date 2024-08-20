1445471cc1cc410f11353577f28972252fd8eac4
Danilo Marra Rabelo <danilo.rabelo@tjdft.jus.br>
2024-08-20 17:07:42 -0300

    feat: Finaliza `layout` usando `tailwind.css`

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      darkBlue: '#071422',
+      profile: ' #0B1B2B',
+      input: '#040F1A',
+      title: '#E7EDF4',
+      text: '#AFC2D4',
+      blue: '#3294F8',
+      label: '#3A536B',
+      span: '#7B96B2',
+      post: '#112131',
+    },
+    fontFamily: {
+      coda: ['Coda', 'sans-serif'],
+      nunito: ['Nunito', 'sans-serif'],
+    },
+    extend: {
+      backgroundImage: {
+        cover: "url('/src/assets/Cover.png')",
+      },
+    },
   },
   plugins: [],
 };
